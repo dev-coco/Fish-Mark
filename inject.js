866656872f6ce74dc49e9c4b65b119b6e4256e70
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-02-20 21:37:58 -0400

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,6 +1,19 @@
-var f = prompt("请输入链接", "");
-var b = f.match(/http.+/g);
-document.write("<script>function test(){var e=document.documentElement.outerHTML.replace(/https.*?[A-Za-z0-9.?/=]+[A-Za-z0-9]/g,\"\");document.open(),document.clear(),document.close(),document.write(e)}</script><div style=\"user-select:none\"><button onclick=\"test()\">clean</button><br></div>");
-for (var i = 0; i < b.length; i++) {
-document.write("<a href=\""+b[i]+"\">"+b[i]+"</a><br>");
+var get_url = prompt("请输入链接", "");
+var get_url_num = get_url.match(/https:\/\/[A-Za-z0-9&/?=.]+[A-Za-z0-9/]/g);
+document.write(`
+               <script>
+                   function test() {
+                       var e = document.documentElement.outerHTML.replace(/https.*?[A-Za-z0-9.?/=]+[A-Za-z0-9]/g, "").replace(/<div class.*?>|<\\/div>|href/g,\"\");;
+                       document.open();
+                       document.clear();
+                       document.close();
+                       document.write(e);
+                   }
+              </script>
+              <p style="user-select:none">
+                  <button onclick="test()">clean</button>
+                  <br>
+              </p>`);
+for (var i = 0; i < get_url_num.length; i++) {
+document.write(`<a href="` + get_url_num[i] + `">` + get_url_num[i] + `</a><br>`);
 }
