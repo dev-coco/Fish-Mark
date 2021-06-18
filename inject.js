2130cf2b1c6a5507b6b5d9b54d322a54e93b1811
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-06-18 00:33:26 -0400

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,19 +1,19 @@
 var get_url = prompt("请输入链接", "");
-var get_url_num = get_url.match(/https:\/\/[A-Za-z0-9&/?=.]+[A-Za-z0-9/]/g);
+var get_url_num = get_url.match(/.+/g);
 document.write(`
                <script>
-                   function test() {
-                       var e = document.documentElement.outerHTML.replace(/https.*?[A-Za-z0-9.?/=]+[A-Za-z0-9]/g, "").replace(/<div class.*?>|<\\/div>|href/g,\"\");;
+                   function clean() {
+                       var e = document.getElementsByClassName('fish_mark')[0].outerText;
                        document.open();
                        document.clear();
                        document.close();
-                       document.write(e);
+                       document.write(e.replace(/http.+/g,"<br>"));
                    }
               </script>
               <p style="user-select:none">
-                  <button onclick="test()">clean</button>
+                  <button onclick="clean()">clean</button>
                   <br>
-              </p>`);
+              </p><div class="fish_mark">`);
 for (var i = 0; i < get_url_num.length; i++) {
 document.write(`<a href="` + get_url_num[i] + `">` + get_url_num[i] + `</a><br>`);
 }
